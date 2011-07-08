3b807ebd0d3be635ef4f5ae5e02e9740c19204d3
liu_kun <liu_kun@msn.com>
2011-07-08 10:22:27 +0800

    修改

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,11 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		int i = 0;
+		int arr[] = new int [10];
+		for( ; i < 10; i++ ){
+			arr[i] = i;
+		}
 		System.out.println( "Hello World!" );
 	}
 }
